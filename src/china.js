c5967458694df8b1b02eab50e81a4ae6cf21efa4
sarasafa <75983100+sarasafa@users.noreply.github.com>
2021-01-26 20:41:38 +0100

    added grades button

--- src/china.js
+++ src/china.js
@@ -4,7 +4,7 @@ function search (event){
   let h1 = document.querySelector("h1");
   h1.innerHTML = cityInput.value;
   let apiKey="6dd93c3c2cfec70598d31537cca13e7f";
-  let units="metric";
+ 
   let city= cityInput.value;
   let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(showTemperature);
@@ -12,8 +12,7 @@ function search (event){
 let searchForm = document.querySelector("#city-search");
 searchForm.addEventListener("submit", search);
 
-
-
+ let units="metric";
 
 function showTemperature(response){
  let tempMax = Math.round (response.data.main.temp_max);
@@ -33,14 +32,14 @@ function showTemperature(response){
  let iconElement=document.querySelector("#icon");
  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
  iconElement.setAttribute("alt", response.data.weather[0].description);
-  console.log(response.data.weather[0].description);
+  
 }
 
 function handlePosition(position){
 let latitude= position.coords.latitude;
 let longitude=position.coords.longitude;
 let apiKey="6dd93c3c2cfec70598d31537cca13e7f";
-let url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+let url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 axios.get(url).then(showTemperature);
 }
 
