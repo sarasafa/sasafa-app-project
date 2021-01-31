52ae921779114b2201b035584c90231bd12edca8
sarasafa <75983100+sarasafa@users.noreply.github.com>
2021-01-31 13:10:29 +0100

    unit conversor added

--- src/china.js
+++ src/china.js
@@ -4,23 +4,26 @@ function search (event){
   let h1 = document.querySelector("h1");
   h1.innerHTML = cityInput.value;
   let apiKey="6dd93c3c2cfec70598d31537cca13e7f";
- 
+
   let city= cityInput.value;
   let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(showTemperature);
 }
+let units="metric";
 let searchForm = document.querySelector("#city-search");
 searchForm.addEventListener("submit", search);
 
- let units="metric";
-
 function showTemperature(response){
- let tempMax = Math.round (response.data.main.temp_max);
- let tempMin = Math.round (response.data.main.temp_min);
+
+ celsiusTemperatureMax=response.data.main.temp_max;
+ celsiusTemperatureMin=response.data.main.temp_min;
+
+ let tempMax = Math.round (celsiusTemperatureMax);
+ let tempMin = Math.round (celsiusTemperatureMin);
  let h2 =document.querySelector("#h2temp");
- h2.innerHTML= tempMax;
+ h2.innerHTML= tempMax + "C";
  let h3 =document.querySelector("#h3temp");
- h3.innerHTML= tempMin;
+ h3.innerHTML= tempMin + "C";
  let h1 = document.querySelector("h1");
  h1.innerHTML = response.data.name;
  let windElement=document.querySelector("#wind");
@@ -32,7 +35,7 @@ function showTemperature(response){
  let iconElement=document.querySelector("#icon");
  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
  iconElement.setAttribute("alt", response.data.weather[0].description);
-  
+ 
 }
 
 function handlePosition(position){
@@ -50,3 +53,39 @@ navigator.geolocation.getCurrentPosition(handlePosition);
 
 let locationer=document.querySelector("#marker");
 locationer.addEventListener("click",retrievePosition);
+
+
+
+
+function showFahrenheit(event){
+  event.preventDefault();
+  
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperatureMax=(celsiusTemperatureMax*9)/5+32;
+  let fahrenheitTemperatureMin=(celsiusTemperatureMin*9)/5+32;
+ 
+  let temperatureElementMax=document.querySelector("#h2temp");
+  temperatureElementMax.innerHTML= Math.round(fahrenheitTemperatureMax)+`F`;
+  let temperatureElementMin=document.querySelector("#h3temp");
+  temperatureElementMin.innerHTML= Math.round(fahrenheitTemperatureMin)+`F`;
+  
+}
+let fahrenheitLink= document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click",showFahrenheit);
+
+function showCelsius(event){
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let temperatureElementMax=document.querySelector("#h2temp");
+  temperatureElementMax.innerHTML= Math.round(celsiusTemperatureMax)+`C`;
+  let temperatureElementMin=document.querySelector("#h3temp");
+  temperatureElementMin.innerHTML= Math.round(celsiusTemperatureMin)+`C`;
+  
+}
+let celsiusTemperatureMax=null;
+let celsiusTemperatureMin=null;
+
+let celsiusLink= document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsius);
